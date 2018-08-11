f0dee99483e8c56496d0f3025267db480df814f1
Horace Keung <horacekeung@hotmail.com>
2018-08-11 05:45:56 +0100

    try ogimage

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,14 @@ module.exports = {
 		script: process.env.DEPLOY_ENV === 'GH_PAGES' ? [{src: '/scrum-poker/gh-spa.js'}] : []
 	},
 	loading: false,
+	meta: {
+		ogImage: {
+			width: 300,
+			height: 300,
+			path: 'https://horacekeung.github.io/scrum-poker/_nuxt/img/qr-code.b12e5f0.png',
+			type: 'image/png'
+		}
+	},
 	manifest: {
 		name: 'Scrum Poker',
 		short_name: 'Scrum Poker',
