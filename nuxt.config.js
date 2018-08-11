06356054a8795f540649922093855d0b0e3cdcb1
Horace Keung <horacekeung@hotmail.com>
2018-08-11 04:43:11 +0100

    use left instead of width

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,7 @@ module.exports = {
 							extractor: TailwindExtractor,
 							extensions: ['vue']
 						}],
-						whitelist: ['html', 'body', 'transition', 'expanded', 'blur', 'menu'],
+						whitelist: ['html', 'body'],
 						whitelistPatterns: [/^btn/]
 					})
 				)
